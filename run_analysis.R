c9d658a0e5045d81ffa08a77c006eb38f5a1564f
ThScheeve <ThScheeve@outlook.com>
2014-04-28 00:14:31 +0200

    Small markup changes

--- run_analysis.R
+++ run_analysis.R
@@ -60,4 +60,4 @@ for (subject in ids) {
 tidy.data.set <- cbind(id.and.activity, means)
 names(tidy.data.set) <- c("subjectID", "activity", sort(c(mean.columns, std.columns)))
 
-write.table(tidy.data.set, "./tidy.txt", sep="\t")
+write.table(tidy.data.set, "./tidy.txt", sep = "\t")
